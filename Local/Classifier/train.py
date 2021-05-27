f66cd38771e56a968f1d0def8289118a1704cf74
Vinoth Kannan <68219125+VinothCruze@users.noreply.github.com>
2021-05-27 11:15:49 +0200

    Update train.py

--- Local/Classifier/train.py
+++ Local/Classifier/train.py
@@ -28,19 +28,7 @@ def load_data(data_path):
 
 
 def prepare_dataset(data_path, test_size=0.2, validation_size=0.2):
-    """Creates train, validation and test sets.
-    :param data_path (str): Path to json file containing data
-    :param test_size (flaot): Percentage of dataset used for testing
-    :param validation_size (float): Percentage of train set used for cross-validation
-    :return X_train (ndarray): Inputs for the train set
-    :return y_train (ndarray): Targets for the train set
-    :return X_validation (ndarray): Inputs for the validation set
-    :return y_validation (ndarray): Targets for the validation set
-    :return X_test (ndarray): Inputs for the test set
-    :return X_test (ndarray): Targets for the test set
-    """
-
-    # load dataset
+   
     X, y = load_data(data_path)
 
     # create train, validation, test split
@@ -63,63 +51,54 @@ def build_model(input_shape, loss="sparse_categorical_crossentropy", learning_ra
     :return model: TensorFlow model
     """
 
-    # build network architecture using convolutional layers
+    
     model = tf.keras.models.Sequential()
 
-    # 1st conv layer
+    
     model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=input_shape,
                                      kernel_regularizer=tf.keras.regularizers.l2(0.001)))
     model.add(tf.keras.layers.BatchNormalization())
     model.add(tf.keras.layers.MaxPooling2D((3, 3), strides=(2,2), padding='same'))
 
-    # 2nd conv layer
+    
     model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu',
                                      kernel_regularizer=tf.keras.regularizers.l2(0.001)))
     model.add(tf.keras.layers.BatchNormalization())
     model.add(tf.keras.layers.MaxPooling2D((3, 3), strides=(2,2), padding='same'))
 
-    # 3rd conv layer
+    
     model.add(tf.keras.layers.Conv2D(32, (2, 2), activation='relu',
                                      kernel_regularizer=tf.keras.regularizers.l2(0.001)))
     model.add(tf.keras.layers.BatchNormalization())
     model.add(tf.keras.layers.MaxPooling2D((2, 2), strides=(2,2), padding='same'))
 
-    # flatten output and feed into dense layer
+    
     model.add(tf.keras.layers.Flatten())
     model.add(tf.keras.layers.Dense(64, activation='relu'))
     tf.keras.layers.Dropout(0.3)
 
-    # softmax output layer
+    
     model.add(tf.keras.layers.Dense(10, activation='softmax'))
 
     optimiser = tf.optimizers.Adam(learning_rate=learning_rate)
 
-    # compile model
+    
     model.compile(optimizer=optimiser,
                   loss=loss,
                   metrics=["accuracy"])
 
-    # print model parameters on console
+    
     model.summary()
 
     return model
 
 
 def train(model, epochs, batch_size, patience, X_train, y_train, X_validation, y_validation):
-    """Trains model
-    :param epochs (int): Num training epochs
-    :param batch_size (int): Samples per batch
-    :param patience (int): Num epochs to wait before early stop, if there isn't an improvement on accuracy
-    :param X_train (ndarray): Inputs for the train set
-    :param y_train (ndarray): Targets for the train set
-    :param X_validation (ndarray): Inputs for the validation set
-    :param y_validation (ndarray): Targets for the validation set
-    :return history: Training history
-    """
+   
 
     earlystop_callback = tf.keras.callbacks.EarlyStopping(monitor="accuracy", min_delta=0.001, patience=patience)
 
-    # train model
+    
     history = model.fit(X_train,
                         y_train,
                         epochs=epochs,
@@ -137,14 +116,14 @@ def plot_history(history):
 
     fig, axs = plt.subplots(2)
 
-    # create accuracy subplot
+    
     axs[0].plot(history.history["accuracy"], label="accuracy")
     axs[0].plot(history.history['val_accuracy'], label="val_accuracy")
     axs[0].set_ylabel("Accuracy")
     axs[0].legend(loc="lower right")
     axs[0].set_title("Accuracy evaluation")
 
-    # create loss subplot
+    
     axs[1].plot(history.history["loss"], label="loss")
     axs[1].plot(history.history['val_loss'], label="val_loss")
     axs[1].set_xlabel("Epoch")
@@ -156,24 +135,24 @@ def plot_history(history):
 
 
 def main():
-    # generate train, validation and test sets
+    
     X_train, y_train, X_validation, y_validation, X_test, y_test = prepare_dataset(DATA_PATH)
 
-    # create network
+    
     input_shape = (X_train.shape[1], X_train.shape[2], 1)
     model = build_model(input_shape, learning_rate=LEARNING_RATE)
 
-    # train network
+    
     history = train(model, EPOCHS, BATCH_SIZE, PATIENCE, X_train, y_train, X_validation, y_validation)
 
-    # plot accuracy/loss for training/validation set as a function of the epochs
+    
     plot_history(history)
 
-    # evaluate network on test set
+    
     test_loss, test_acc = model.evaluate(X_test, y_test)
     print("\nTest loss: {}, test accuracy: {}".format(test_loss, 100*test_acc))
 
-    # save model
+    
     model.save(SAVED_MODEL_PATH)
 
 
