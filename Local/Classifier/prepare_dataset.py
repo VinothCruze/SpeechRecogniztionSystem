af5b5ea56ab1084bc930bdc6387500b4869aee6a
Vinoth Kannan <68219125+VinothCruze@users.noreply.github.com>
2021-05-27 11:14:27 +0200

    Update prepare_dataset.py

--- Local/Classifier/prepare_dataset.py
+++ Local/Classifier/prepare_dataset.py
@@ -8,16 +8,7 @@ SAMPLES_TO_CONSIDER = 22050  # 1 sec. of audio
 
 
 def preprocess_dataset(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_length=512):
-    """Extracts MFCCs from music dataset and saves them into a json file.
-    :param dataset_path:
-    :param json_path:
-    :param num_mfcc:
-    :param n_fft:
-    :param hop_length:
-    :return:
-    """
-
-    # dictionary where we'll store mapping, labels, MFCCs and filenames
+  
     data = {
         "mapping": [],
         "labels": [],
@@ -25,41 +16,41 @@ def preprocess_dataset(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_len
         "files": []
     }
 
-    # loop through all sub-dirs
+    
     for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
 
-        # ensure we're at sub-folder level
+       
         if dirpath is not dataset_path:
 
-            # save label (i.e., sub-folder name) in the mapping
+           
             label = dirpath.split("/")[-1]
             print(label)
             data["mapping"].append(label)
             print("\nProcessing: '{}'".format(label))
 
-            # process all audio files in sub-dir and store MFCCs
+            s
             for f in filenames:
                 file_path = os.path.join(dirpath, f)
 
-                # load audio file and slice it to ensure length consistency among different files
+                
                 signal, sample_rate = librosa.load(file_path)
 
-                # drop audio files with less than pre-decided number of samples
+               
                 if len(signal) >= SAMPLES_TO_CONSIDER:
-                    # ensure consistency of the length of the signal
+                    
                     signal = signal[:SAMPLES_TO_CONSIDER]
 
-                    # extract MFCCs
+                    
                     MFCCs = librosa.feature.mfcc(signal, sample_rate, n_mfcc=num_mfcc, n_fft=n_fft,
                                                  hop_length=hop_length)
 
-                    # store data for analysed track
+                    
                     data["MFCCs"].append(MFCCs.T.tolist())
                     data["labels"].append(i - 1)
                     data["files"].append(file_path)
                     print("{}: {}".format(file_path, i - 1))
 
-    # save data in json file
+   
     with open(json_path, "w") as fp:
         json.dump(data, fp, indent=4)
 
